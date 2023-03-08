714a058a059fd288da13d3cb164d5fb2049a830d
leesub0168 <53374789+leesub0168@users.noreply.github.com>
2023-03-09 02:06:09 +0900

    [Silver II] Title: 좌표 압축, Time: 1876 ms, Memory: 198420 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+class Test {
+    public void solution(BufferedReader br) throws Exception {
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        String[] srr = br.readLine().split(" ");
+        for(int i=0;i<n;i++) {
+            arr[i] = Integer.parseInt(srr[i]);
+        }
+        
+        int[] sortArr = Arrays.copyOf(arr, n);
+        Arrays.sort(sortArr);
+        
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<n;i++) {
+            if(i == 0 || sortArr[i-1] != sortArr[i]) list.add(sortArr[i]);
+        }
+        
+        Integer[] removeDupl = list.toArray(new Integer[0]);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<n;i++) {
+            sb.append(search(removeDupl, arr[i])).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+    public int search(Integer[] arr, int target) {
+        int st = 0;
+        int end = arr.length -1;
+        while(st <= end) {
+            int mid = (st + end) / 2;
+            if(arr[mid] == target) return mid;
+            else if(arr[mid] > target) {
+                end = mid - 1;
+            }else {
+                st = mid + 1;
+            }
+        }
+        return 0;
+    }
+}
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Test test = new Test();
+        test.solution(br);
+    }
+}
