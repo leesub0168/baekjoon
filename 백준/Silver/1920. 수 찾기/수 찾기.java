e12482e99523fa486518bb9c4961b5532f397f8f
leesub0168 <53374789+leesub0168@users.noreply.github.com>
2023-03-08 23:57:28 +0900

    [Silver IV] Title: 수 찾기, Time: 584 ms, Memory: 42640 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,8 +1,9 @@
 import java.util.*;
 import java.io.*;
 
-class BinarySearch {
-    public void solution(BufferedReader br) throws Exception {
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,32 +17,9 @@ class BinarySearch {
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<m;i++) {
             int target = Integer.parseInt(st.nextToken());
-            sb.append(search(arr,target));
-            if(i != m-1) sb.append("\n");
+            if(Arrays.binarySearch(arr, target) >= 0) sb.append("1\n");
+            else sb.append("0\n");
         }
         System.out.print(sb.toString());
-    }
-    
-    public int search(int[] arr, int target) {
-        int st = 0;
-        int end = arr.length - 1;
-        while(st <= end) {
-            int mid = (st + end)/2;
-            if(arr[mid] == target) return 1;
-            else if(arr[mid] > target) {
-                end = mid - 1;
-            }else {
-                st = mid + 1;
-            }
-        }
-        return 0;
-    }
-}
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BinarySearch bs = new BinarySearch();
-        bs.solution(br);
     }
 }
