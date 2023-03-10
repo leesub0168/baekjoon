0cfc7c8addb27f55abef23f384cb13510f0badcd
leesub0168 <53374789+leesub0168@users.noreply.github.com>
2023-03-10 21:22:09 +0900

    [Gold IV] Title: 세 수의 합, Time: 272 ms, Memory: 24008 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -11,17 +11,18 @@ public class Main {
         }
         Arrays.sort(arr);
         
-        List<Integer> list = new ArrayList<>();
+        int[] sum = new int[n * n];
+        int idx = 0;
         for(int i=0;i<n;i++) {
             for(int j=i;j<n;j++) {
-                list.add(arr[i] + arr[j]);
+                sum[idx++] = arr[i] + arr[j];
             }
         }
-        Collections.sort(list);
+        Arrays.sort(sum, 0, idx-1);
         
         for(int i=n-1;i>=0;i--) {
             for(int j=i;j>=0;j--) {
-                if(Collections.binarySearch(list, arr[i] - arr[j]) >= 0) {
+                if(Arrays.binarySearch(sum, 0, idx - 1, arr[i] - arr[j]) >= 0) {
                     System.out.println(arr[i]);
                     return;
                 }
