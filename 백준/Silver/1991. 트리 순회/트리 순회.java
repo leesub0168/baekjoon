7f2963ebc0bc19111ff1fdeb392f48f0256f9f21
leesub0168 <53374789+leesub0168@users.noreply.github.com>
2023-03-22 00:44:15 +0900

    [Silver I] Title: 트리 순회, Time: 120 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -2,41 +2,39 @@ import java.util.*;
 import java.io.*;
 
 class Test {
-    int[] lc = new int[30];
-    int[] rc = new int[30];
+    char[] lc = new char[30];
+    char[] rc = new char[30];
     StringBuilder sb = new StringBuilder();
     public void solution(BufferedReader br) throws Exception {
         int n = Integer.parseInt(br.readLine());
         for(int i=0;i<n;i++) {
             char[] chars = br.readLine().toCharArray();
-            int parent = chars[0] - 'A' + 1;
-            int node1 = (chars[2] == '.')?0:(chars[2] - 'A' + 1);
-            int node2 = (chars[4] == '.')?0:(chars[4] - 'A' + 1);
-            lc[parent] = node1;
-            rc[parent] = node2;
+            int parent = chars[0] - 'A';
+            lc[parent] = chars[2];
+            rc[parent] = chars[4];
         }
-        preorder(1);
+        preorder('A');
         sb.append("\n");
-        inOrder(1);
+        inOrder('A');
         sb.append("\n");
-        postOrder(1);
+        postOrder('A');
         System.out.println(sb);
     }
     
-    public void preorder(int cur) {
-        sb.append(new String(Character.toChars(cur + 'A' -1)));
-        if(lc[cur] != 0) preorder(lc[cur]);
-        if(rc[cur] != 0) preorder(rc[cur]);
+    public void preorder(char cur) {
+        sb.append(cur);
+        if(lc[cur-'A'] != '.') preorder(lc[cur-'A']);
+        if(rc[cur-'A'] != '.') preorder(rc[cur-'A']);
     }
-    public void inOrder(int cur) {
-        if(lc[cur] != 0) inOrder(lc[cur]);
-        sb.append(new String(Character.toChars(cur + 'A' -1)));
-        if(rc[cur] != 0) inOrder(rc[cur]);
+    public void inOrder(char cur) {
+        if(lc[cur-'A'] != '.') inOrder(lc[cur-'A']);
+        sb.append(cur);
+        if(rc[cur-'A'] != '.') inOrder(rc[cur-'A']);
     }
-    public void postOrder(int cur) {
-        if(lc[cur] != 0) postOrder(lc[cur]);
-        if(rc[cur] != 0) postOrder(rc[cur]);
-        sb.append(new String(Character.toChars(cur + 'A' -1)));
+    public void postOrder(char cur) {
+        if(lc[cur-'A'] != '.') postOrder(lc[cur-'A']);
+        if(rc[cur-'A'] != '.') postOrder(rc[cur-'A']);
+        sb.append(cur);
     }
 }
 public class Main {
