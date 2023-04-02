3497d9b3ff7a8152af27a9605e4fc940c1fa5c7b
leesub0168 <53374789+leesub0168@users.noreply.github.com>
2023-04-02 15:42:33 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        long a = Long.valueOf(st.nextToken());
-        long b = Long.valueOf(st.nextToken());
-        long c = Long.valueOf(st.nextToken());
-        System.out.println(a + b + c);
+        long sum = Long.valueOf(st.nextToken());
+        sum += Long.valueOf(st.nextToken());
+        sum += Long.valueOf(st.nextToken());
+        System.out.println(sum);
     }
 }
