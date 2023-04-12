f4334468e17c07d1dea2e785a97726455d7ccfbe
leesub0168 <53374789+leesub0168@users.noreply.github.com>
2023-04-13 01:08:40 +0900

    [Bronze I] Title: 단어 공부, Time: 300 ms, Memory: 24996 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        str = str.toUpperCase();
+        int[] arr = new int[26];
+        
+        for(int i=0;i<str.length();i++) {
+            arr[str.charAt(i) - 'A']++;
+        }
+        
+        int max = -1;
+        int idx = -1;
+        int[] counts = new int[1000000];
+        for(int i=0; i < 26; i++) {
+            if(arr[i] > 0 ) {
+                counts[arr[i]]++;
+                if(arr[i] > max) {
+                    max = arr[i];
+                    idx = i + 'A';
+                }
+            }
+        }
+        if(counts[max] > 1) System.out.println("?");
+        else System.out.println((char) idx);
+        
+    }
+}
