51cb20472acad0dce773013cbd9c76288d110420
leesub0168 <53374789+leesub0168@users.noreply.github.com>
2023-04-24 23:00:46 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 18108 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -8,13 +8,9 @@ public class Main {
         
         int[] arr = new int[n+1];
         for(int i=2; i <= n; i++) {
-            int divide_3 = Integer.MAX_VALUE;
-            int divide_2 = Integer.MAX_VALUE;
-            int minus_1 = arr[i-1] + 1;
-            if(i % 3 == 0) divide_3 = arr[i / 3] + 1;
-            if(i % 2 == 0) divide_2 = arr[i / 2] + 1;
-            
-            arr[i] = Math.min(Math.min(divide_3, divide_2), minus_1);
+            arr[i] = arr[i-1] + 1;
+            if(i % 3 == 0) arr[i] = Math.min(arr[i / 3] + 1, arr[i]);
+            if(i % 2 == 0) arr[i] = Math.min(arr[i / 2] + 1, arr[i]);
         }
         System.out.println(arr[n]);
     }
