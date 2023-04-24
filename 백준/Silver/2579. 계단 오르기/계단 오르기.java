46415d65a877fb0c993bdf577af7765ad5628eb2
leesub0168 <53374789+leesub0168@users.noreply.github.com>
2023-04-25 01:02:45 +0900

    [Silver III] Title: 계단 오르기, Time: 120 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -11,17 +11,20 @@ public class Main {
         for(int i=1; i <= n; i++) {
             score[i] = Integer.parseInt(br.readLine());
         }
+        if(n == 1) {
+            System.out.println(score[n]);
+        }else {
+            arr[1][1] = score[1];
+            arr[1][2] = 0;
+            arr[2][1] = score[2];
+            arr[2][2] = score[1] + score[2];
         
-        arr[1][1] = score[1];
-        arr[1][2] = 0;
-        arr[2][1] = score[2];
-        arr[2][2] = score[1] + score[2];
-        
-        for(int i = 3; i <= n; i++) {
-            arr[i][1] = Math.max(arr[i-2][1], arr[i-2][2]) + score[i];
-            arr[i][2] = arr[i-1][1] + score[i];
+            for(int i = 3; i <= n; i++) {
+                arr[i][1] = Math.max(arr[i-2][1], arr[i-2][2]) + score[i];
+                arr[i][2] = arr[i-1][1] + score[i];
+            }
+            System.out.println(Math.max(arr[n][1], arr[n][2]));
         }
-        System.out.println(Math.max(arr[n][1], arr[n][2]));
         
     }
 }
