aa43e0471188616e8ec1f78f3d399ce8d1cbc160
leesub0168 <53374789+leesub0168@users.noreply.github.com>
2023-04-26 01:56:26 +0900

    [Silver I] Title: 1로 만들기 2, Time: 160 ms, Memory: 23604 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        int[] arr = new int[n + 5];
+        int[] step = new int[n + 5];
+        
+        arr[2] = 1;
+        arr[3] = 1;
+        
+        step[2] = 1;
+        step[3] = 1;
+        
+        for(int i=4; i <= n; i++) {
+            arr[i] = arr[i-1] + 1;
+            step[i] = i-1;
+            
+            if(i % 3 == 0 && arr[i] > arr[i / 3] + 1) {
+                arr[i] = arr[i / 3] + 1;
+                step[i] = i / 3;
+            }
+            if(i % 2 == 0 && arr[i] > arr[i / 2] + 1) {
+                arr[i] = arr[i / 2] + 1;
+                step[i] = i / 2;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(arr[n] + "\n");
+        sb.append(n + " ");
+        
+        while (n > 1) {
+            sb.append(step[n]+ " ");
+            n = step[n];
+        }
+        System.out.println(sb);
+    }
+}
