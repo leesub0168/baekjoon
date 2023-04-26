eced4d6c081ecb7af4739aeac76ec16fea4a5fcc
leesub0168 <53374789+leesub0168@users.noreply.github.com>
2023-04-27 01:33:29 +0900

    [Silver III] Title: 피보나치 함수, Time: 152 ms, Memory: 16036 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -5,25 +5,21 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int tc = Integer.parseInt(br.readLine());
-        int[] arr = new int[42];
+        int[][] arr = new int[42][2];
         
-        arr[1] = 1;
-        arr[2] = 1;
+        arr[0][0] = 1;
+        arr[1][1] = 1;
         
-        for(int i = 3; i < arr.length; i++) {
-            arr[i] = arr[i-1] + arr[i-2];
+        for(int i = 2; i <= 40; i++) {
+            arr[i][0] = arr[i-1][0] + arr[i-2][0];
+            arr[i][1] = arr[i-1][1] + arr[i-2][1];
         }
         
         
         StringBuilder sb = new StringBuilder();
         while(tc > 0) {
             int n = Integer.parseInt(br.readLine());
-            
-            if(n == 0) {
-                sb.append("1 0\n");
-            }else {
-                sb.append(arr[n-1] + " " + arr[n] + "\n");
-            }
+            sb.append(arr[n][0] + " " + arr[n][1] + "\n");
             
             tc--;
         }
