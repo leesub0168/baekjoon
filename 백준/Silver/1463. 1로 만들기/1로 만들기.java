a21add556293b4fe2a25bc1ae4ac6afdf6d79400
leesub0168 <53374789+leesub0168@users.noreply.github.com>
2023-04-28 21:42:43 +0900

    [Silver III] Title: 1로 만들기, Time: 104 ms, Memory: 15504 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -6,9 +6,10 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         
-        int[] arr = new int[n+1];
-        for(int i=2; i <= n; i++) {
-            arr[i] = arr[i-1] + 1;
+        int[] arr = new int[n+5];
+        arr[2] = 1;
+        for(int i=3; i <= n; i++) {
+            arr[i] = arr[i - 1] + 1;
             if(i % 3 == 0) arr[i] = Math.min(arr[i / 3] + 1, arr[i]);
             if(i % 2 == 0) arr[i] = Math.min(arr[i / 2] + 1, arr[i]);
         }
