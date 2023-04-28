b47f8216c550cf696d3fd7235b3ac221602e7452
leesub0168 <53374789+leesub0168@users.noreply.github.com>
2023-04-28 22:10:52 +0900

    [Silver I] Title: RGB거리, Time: 88 ms, Memory: 12172 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,31 +1,34 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int cnt = Integer.parseInt(br.readLine());
-        int[][] arr = new int[cnt+1][3];
-        int[] red = new int[cnt];
-        int[] green = new int[cnt];
-        int[] blue = new int[cnt];
+        int n = Integer.parseInt(br.readLine());
         
-        for(int i = 0; i < cnt; i++) {
+        int[][] arr = new int[n+1][3];
+        int[] red = new int[n+1];
+        int[] green = new int[n+1];
+        int[] blue = new int[n+1];
+        
+        for(int i=1; i <= n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
+            
             red[i] = Integer.parseInt(st.nextToken());
             green[i] = Integer.parseInt(st.nextToken());
             blue[i] = Integer.parseInt(st.nextToken());
         }
         
-        arr[1][0] = red[0];
-        arr[1][1] = green[0];
-        arr[1][2] = blue[0];
+        arr[1][0] = red[1];
+        arr[1][1] = green[1];
+        arr[1][2] = blue[1];
         
-        for(int i = 2; i <= cnt; i++) {
-            arr[i][0] = Math.min(arr[i-1][1], arr[i-1][2]) + red[i-1];
-            arr[i][1] = Math.min(arr[i-1][0], arr[i-1][2]) + green[i-1];
-            arr[i][2] = Math.min(arr[i-1][0], arr[i-1][1]) + blue[i-1];
+        for(int i=2; i <= n; i++) {
+            arr[i][0] = Math.min(arr[i-1][1], arr[i-1][2]) + red[i];
+            arr[i][1] = Math.min(arr[i-1][0], arr[i-1][2]) + green[i];
+            arr[i][2] = Math.min(arr[i-1][0], arr[i-1][1]) + blue[i];
         }
-        System.out.println(Math.min(Math.min(arr[cnt][0], arr[cnt][1]),arr[cnt][2]));
+        
+        System.out.println(Math.min(Math.min(arr[n][0], arr[n][1]),arr[n][2]));
     }
 }
