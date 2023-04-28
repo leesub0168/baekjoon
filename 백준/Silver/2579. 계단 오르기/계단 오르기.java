8925b16faa2fb4f767d8d2cb0f86366779a21b03
leesub0168 <53374789+leesub0168@users.noreply.github.com>
2023-04-28 23:04:55 +0900

    [Silver III] Title: 계단 오르기, Time: 80 ms, Memory: 11584 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,30 +1,30 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[301][3];
-        int[] score = new int[301];
         
+        int[] arr = new int[n+5];
+        int[] score = new int[n+5];
+        int total = 0;
         for(int i=1; i <= n; i++) {
             score[i] = Integer.parseInt(br.readLine());
+            total += score[i];
         }
-        if(n == 1) {
-            System.out.println(score[n]);
+        if(n < 3) {
+            System.out.println(total);
         }else {
-            arr[1][1] = score[1];
-            arr[1][2] = 0;
-            arr[2][1] = score[2];
-            arr[2][2] = score[1] + score[2];
+            arr[1] = score[1];
+            arr[2] = score[2];
+            arr[3] = score[3];
         
-            for(int i = 3; i <= n; i++) {
-                arr[i][1] = Math.max(arr[i-2][1], arr[i-2][2]) + score[i];
-                arr[i][2] = arr[i-1][1] + score[i];
+            for(int i=4; i <= n; i++) {
+                arr[i] = Math.min(arr[i-2], arr[i-3]) + score[i];
             }
-            System.out.println(Math.max(arr[n][1], arr[n][2]));
-        }
         
+            System.out.println(total - Math.min(arr[n-1], arr[n-2]));
+        }
     }
 }
