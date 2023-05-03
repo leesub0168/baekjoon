845990575dc5adfbd014516388ad423168790f07
leesub0168 <53374789+leesub0168@users.noreply.github.com>
2023-05-04 02:07:22 +0900

    [Silver I] Title: 미로 탐색, Time: 192 ms, Memory: 16604 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,11 +1,11 @@
 import java.util.*;
 import java.io.*;
 
-class EntryPair {
+class Pair {
     int x;
     int y;
 
-    public EntryPair(int x, int y)  {
+    public Pair(int x, int y)  {
         this.x = x;
         this.y = y;
     }
@@ -13,47 +13,47 @@ class EntryPair {
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st1.nextToken());
-        int m = Integer.parseInt(st1.nextToken());
-
-        int[][] board = new int[n+1][m+1];
-        boolean[][] vis = new boolean[n+1][m+1];
-
-        for(int i=0;i<n;i++) {
-            String s = br.readLine();
-            for(int j=0;j<m;j++) {
-                board[i][j] = Character.getNumericValue(s.charAt(j));
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][m];
+        int[][] dist = new int[n][m];
+        
+        for(int i = 0; i < n; i++) {
+            String[] srr = br.readLine().split("");
+            Arrays.fill(dist[i], -1);
+            
+            for(int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(srr[j]);
+                
+                if(arr[i][j] == 1) dist[i][j] = 0;
             }
         }
-        Queue<EntryPair> deque = new LinkedList<>();
-        deque.add(new EntryPair(0,0));
-        vis[0][0] = true;
-
-        int[] dx = {1,0,-1,0};
-        int[] dy = {0,1,0,-1};
-
-        while (!deque.isEmpty()) {
-            EntryPair pair = deque.remove();
-            for(int dir=0;dir<4;dir++) {
-                int xx = pair.x + dx[dir];
-                int yy = pair.y + dy[dir];
-
-                if(xx >= 0 && yy >= 0 && xx < m && yy < n) {
-                    if(board[yy][xx] != 0 && !vis[yy][xx]) {
-                        vis[yy][xx] = true;
-                        board[yy][xx] = board[pair.y][pair.x] + 1;
-                        deque.add(new EntryPair(xx,yy));
-                    }
+        
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+        
+        Queue<Pair> queue = new ArrayDeque<>();
+        dist[0][0] = 1;
+        queue.add(new Pair(0,0));
+        
+        while(!queue.isEmpty()) {
+            Pair pair = queue.poll();
+            
+            for(int i = 0; i < 4; i++) {
+                int nx = pair.x + dx[i];
+                int ny = pair.y + dy[i];
+                
+                if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                if(arr[nx][ny] == 1 && dist[nx][ny] == 0) {
+                    dist[nx][ny] = dist[pair.x][pair.y] + 1;
+                    queue.add(new Pair(nx, ny));
                 }
             }
         }
-
-        bw.write(board[n-1][m-1]+"");
-        bw.flush();
-        bw.close();
+        
+        System.out.println(dist[n-1][m-1]);
         
     }
 }
