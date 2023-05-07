7ab0dfc113f01b8cd50ba69ca6438ced71d52f31
leesub0168 <53374789+leesub0168@users.noreply.github.com>
2023-05-07 23:37:51 +0900

    [Silver I] Title: 그림, Time: 488 ms, Memory: 54228 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,10 +1,9 @@
 import java.util.*;
 import java.io.*;
-
 class Pair {
     int x;
     int y;
-
+    
     public Pair(int x, int y) {
         this.x = x;
         this.y = y;
@@ -13,54 +12,57 @@ class Pair {
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
         
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st1.nextToken());
-        int m = Integer.parseInt(st1.nextToken());
-
+        
         int[][] arr = new int[n][m];
-        for(int i=0;i<n;i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j=0;j<m;j++) {
+        boolean[][] vis = new boolean[n][m];
+        
+        for(int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            
+            for(int j=0; j < m; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        boolean[][] vis = new boolean[n][m];
-
         int[] dx = {1,0,-1,0};
         int[] dy = {0,1,0,-1};
+        
         int mx = 0;
-        int num = 0;
-
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
-                if(arr[i][j] == 0 || vis[i][j]) continue;
-                num++;
-
+        int cnt = 0;
+        
+        for(int i=0; i < n; i++) {
+            for(int j=0; j < m; j++) {
+                if(vis[i][j] || arr[i][j] == 0) continue;
+                
+                cnt++;
                 Queue<Pair> queue = new ArrayDeque<>();
-                vis[i][j] = true;
                 queue.add(new Pair(i,j));
+                vis[i][j] = true;
                 int area = 0;
-                while (!queue.isEmpty()) {
+                while(!queue.isEmpty()) {
                     area++;
-                    Pair pair =  queue.poll();
-
-                    for(int dir=0;dir<4;dir++) {
-                        int nx = pair.x + dx[dir];
-                        int ny = pair.y + dy[dir];
-                        if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                    Pair pair = queue.poll();
+                    
+                    for(int k=0; k < 4; k++) {
+                        int nx = pair.x + dx[k];
+                        int ny = pair.y + dy[k];
+                        
+                        if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
                         if(vis[nx][ny] || arr[nx][ny] != 1) continue;
                         vis[nx][ny] = true;
-                        queue.add(new Pair(nx,ny));
+                        queue.add(new Pair(nx, ny));
+                        
                     }
+                    
                 }
-
                 if(mx < area) mx = area;
+                
             }
         }
-
-        System.out.println(num);
+        System.out.println(cnt);
         System.out.println(mx);
     }
 }
